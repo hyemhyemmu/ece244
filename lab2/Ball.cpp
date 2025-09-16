4c5ffd7c3c1d67fdea359a351f770619f10e7104
Daniel <mofei263@outlook.com>
2025-09-15 22:38:11 -0400

    update Ball.cpp and Player.cpp

--- lab2/Ball.cpp
+++ lab2/Ball.cpp
@@ -2,4 +2,32 @@
 //  Ball.cpp
 //  Lab 2 Pong Game
 //
-//  Created by Author Name, Date
+//  Created by Fei Mo, 2025/9/15
+
+#include "Ball.h"
+
+Ball::Ball() {}
+
+Ball::Ball(double x, double y, double velocity_x, double velocity_y, int id) {
+  this->x = x;
+  this->y = y;
+  this->velocity_x = velocity_x;
+  this->velocity_y = velocity_y;
+  this->id = id;
+  this->width = 1.0;
+  this->height = 1.0;
+}
+
+double Ball::getX() { return x; }
+
+int Ball::getID() { return id; }
+
+void Ball::update() {
+  velocity_y = velocity_y - 0.02 * timeStep;  // gravity
+
+  // displacement
+  x = x + velocity_x * timeStep;
+  y = y + velocity_y * timeStep;
+}
+
+// TODO: overlap function
--- lab2/Player.cpp
+++ lab2/Player.cpp
@@ -2,4 +2,52 @@
 //  Player.cpp
 //  Lab 2 Pong Game
 //
-//  Created by Author Name, Date
+//  Created by Fei Mo, 2025/9/15
+
+#include "Player.h"
+
+Player::Player() {}
+
+Player::Player(double x, double y, int height) {
+  this->x = x;
+  this->y = y;
+  this->height = height;
+  this->width = 1;
+}
+
+double Player::getX() { return x; }
+
+double Player::getY() { return y; }
+
+int Player::getHeight() { return height; }
+
+int Player::getWidth() { return width; }
+
+void Player::decreaseHeight(int delta_to_decrease_by) {
+  int newHeight = height - delta_to_decrease_by;
+  height = (newHeight < 3) ? 3 : newHeight;
+}
+
+void Player::update(char c) {
+  if (c == 'A') {  // up
+    y += 2;
+
+    if (y + height > HEIGHT - 1) {
+      y = HEIGHT - 1 - height;  // up max
+    }
+  } else if (c == 'B') {  // down
+    y -= 2;
+
+    if (y < 0) {
+      y = 0;  // down max
+    }
+  }
+}
+
+void Player::draw(Screen& screen_to_draw_to) {
+  for (int j = y; j <= y + height; ++j) {
+    for (int i = x; i <= x + width; ++i) {
+      screen_to_draw_to.addPixel(x, y, '#');
+    }
+  }
+}
