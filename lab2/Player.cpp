da62c241c2293089af1043f40d640be618f6f0f3
Daniel <mofei263@outlook.com>
2025-09-18 22:47:08 -0400

    update Player.cpp

--- lab2/Player.cpp
+++ lab2/Player.cpp
@@ -45,9 +45,7 @@ void Player::update(char c) {
 }
 
 void Player::draw(Screen& screen_to_draw_to) {
-  for (int j = y; j <= y + height; ++j) {
-    for (int i = x; i <= x + width; ++i) {
-      screen_to_draw_to.addPixel(i, j, '#');
-    }
+  for (int j = y; j < y + height; ++j) {
+    screen_to_draw_to.addPixel(x, j, '#');
   }
 }
